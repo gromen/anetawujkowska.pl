b6e3a9753b47ccc140d9baa9d8477c442d108dbe
Marcin Gromek <gromson101@gmail.com>
2026-05-26 20:41:15 +0200

    refactor: update Cloudflare adapter configuration and enhance index.astro content
    - Modified the Cloudflare adapter configuration to include imageService for improved image handling.
    - Updated text in index.astro for clarity, including changes to meeting descriptions and personal meeting options.
    - Adjusted global styles for better text alignment on smaller screens.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ export default defineConfig({
   site: 'https://anetawujkowska.pl',
   trailingSlash: 'ignore',
   integrations: [sitemap()],
-  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
-  adapter: cloudflare()
+  adapter: cloudflare({
+    imageService: 'compile',
+  }),
 });
